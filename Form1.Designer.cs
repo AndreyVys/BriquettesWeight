227f326cdfebad3f226c38e1ca14cbeace18b971
Andrey.wys <andrey.wys@gmail.com>
2024-02-01 23:17:14 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace BriquettesWeight
+{
+    partial class BriquettesWeight
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BriquettesWeight));
+            this.labelWeight = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelWeight
+            // 
+            this.labelWeight.AutoSize = true;
+            this.labelWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWeight.Location = new System.Drawing.Point(72, 37);
+            this.labelWeight.Name = "labelWeight";
+            this.labelWeight.Size = new System.Drawing.Size(0, 52);
+            this.labelWeight.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(197, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "кг";
+            // 
+            // BriquettesWeight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 103);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelWeight);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "BriquettesWeight";
+            this.Text = "Масса брикетов";
+            this.Load += new System.EventHandler(this.BriquettesWeight_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWeight;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Net.Sockets;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace BriquettesWeight
+{
+    public partial class BriquettesWeight : Form
+    {
+
+        public BriquettesWeight()
+        {
+            InitializeComponent();
+        }
+
+        private void BriquettesWeight_Load(object sender, EventArgs e)
+
+        {
+            this.Activated += AfterLoading;
+        }
+
+        private void AfterLoading(object sender, EventArgs e)
+        {
+            this.Activated -= AfterLoading;
+            Connect("192.168.77.21", 4001);
+        }
+        async void  Connect(String server, Int32 port)
+        {
+            try
+            {
+
+
+                //TcpClient tcpClient = new TcpClient();
+                //tcpClient.ConnectAsync("192.168.77.21", 4001);
+                //NetworkStream stream = tcpClient.GetStream();
+                //byte[] data = new byte[512];
+                //var response = new StringBuilder();
+                //string results;
+
+                //do
+                //{
+                //    int len = stream.Read(data, 0, 12);
+                //    response.Append(Encoding.ASCII.GetString(data, 0, len));
+                //    Regex rg = new Regex(@"ww(.*?)kg"); //  = ;
+                //    results = rg.Match(response.ToString()).Groups[1].Value;
+                //}
+
+
+
+
+                Socket mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                mySocket.Connect(server, port);
+                while (true)
+                {
+                    Stream stream = new NetworkStream(mySocket);
+                Byte[] responseData = new byte[12];
+                int bytes;
+                var response = new StringBuilder();
+
+                    string result="";
+                    responseData.Initialize();
+                    do
+                    {
+                        bytes = await stream.ReadAsync(responseData, 0, 12);
+                        response.Append(Encoding.ASCII.GetString(responseData, 0, bytes));
+                        Regex rg = new Regex(@"ww(.*?)kg"); //  = ;
+                        result = rg.Match(response.ToString()).Groups[1].Value;
+                    }
+                    while (result == "");
+                    result = result.TrimStart('0');
+                    result = result.Length > 0 ? result : "0";
+                    labelWeight.Text = result;
+                    stream.Close();
+                    // Refresh();
+
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+
+                // MessageBox.Show("ArgumentNullException: {0}" + e.ToString());
+                labelWeight.Text =  e.ToString();
+            }
+            catch (SocketException e)
+            {
+                // MessageBox.Show("SocketException: {0}" + e.ToString());
+                labelWeight.Text = e.ToString();
+            }
+        }
+
+    }
+}
